ff89e1d136ef7fb1655c9e128569c650c89c1cab
Ariel Kanamori <ari.kanamori@gmail.com>
2015-06-18 10:40:25 -0700

    adds strong params for article in articles_helper.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
+
 	def index
 		@articles = Article.all
 	end
@@ -11,10 +13,10 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 	def create
-		@article = Article.new(
-  			title: params[:article][:title],
-  			body: params[:article][:body])
-  		@article.save
+		@article = Article.new(article_params)
+		@article.save
+  		
   		redirect_to article_path(@article)
 	end	
+
 end
